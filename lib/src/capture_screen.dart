2747730e3b1985cc1d6e78ff12b0cfab6d3a728d
Viktor Kifer <viktor.kifer.dev@gmail.com>
2020-01-24 12:49:49 +0200

    Create ANDROID screenshots using ADB in demo mode and without animations

--- lib/src/capture_screen.dart
+++ lib/src/capture_screen.dart
@@ -15,11 +15,38 @@ Future screenshot(final driver, Config config, String name,
       await driver.waitUntilNoTransientCallbacks(timeout: timeout);
     }
 
-    final pixels = await driver.screenshot();
+    final deviceType = (await config.screenshotsEnv)["device_type"];
+    final isAndroid = deviceType == "android";
+
     final testDir = '${config.stagingDir}/$kTestScreenshotsDir';
-    final file =
-        await File('$testDir/$name.$kImageExtension').create(recursive: true);
-    await file.writeAsBytes(pixels);
+    final file = await File('$testDir/$name.$kImageExtension').create(recursive: true);
+
+    if (isAndroid) {
+      print("Creating screenshot using ADB");
+      await Process.run('adb', ['shell', 'settings', 'put', 'global', 'window_animation_scale', '0.0']);
+      await Process.run('adb', ['shell', 'settings', 'put', 'global', 'transition_animation_scale', '0.0']);
+      await Process.run('adb', ['shell', 'settings', 'put', 'global', 'animator_duration_scale', '0.0']);
+      await Process.run('adb', ['shell', 'settings', 'put', 'global', 'sysui_demo_allowed', '1']);
+      await Process.run('adb', ['shell', 'am', 'broadcast', '-a', 'com.android.systemui.demo', '-e', 'command', 'enter']);
+      await Process.run('adb', ['shell', 'am', 'broadcast', '-a', 'com.android.systemui.demo', '-e', 'command', 'clock', '-e', 'hhmm', '0800']);
+      await Process.run('adb', ['shell', 'am', 'broadcast', '-a', 'com.android.systemui.demo', '-e', 'command', 'battery', '-e', 'level', '100']);
+      await Process.run('adb', ['shell', 'am', 'broadcast', '-a', 'com.android.systemui.demo', '-e', 'command', 'network', '-e', 'wifi', 'show', '-e', 'level', '4']);
+      await Process.run('adb', ['shell', 'am', 'broadcast', '-a', 'com.android.systemui.demo', '-e', 'command', 'network', '-e', 'mobile', 'show', '-e', 'datatype', 'lte', '-e', 'level', '4']);
+      await Process.run('adb', ['shell', 'am', 'broadcast', '-a', 'com.android.systemui.demo', '-e', 'command', 'notifications', '-e', 'visible', 'false']);
+
+      final screenshotResult = await Process.run('adb', ['exec-out', 'screencap', '-p'], stdoutEncoding: null);
+      await file.writeAsBytes(screenshotResult.stdout);
+
+      await Process.run('adb', ['shell', 'am', 'broadcast', '-a', 'com.android.systemui.demo', '-e', 'command', 'exit']);
+      await Process.run('adb', ['shell', 'settings', 'put', 'global', 'window_animation_scale', '1.0']);
+      await Process.run('adb', ['shell', 'settings', 'put', 'global', 'transition_animation_scale', '1.0']);
+      await Process.run('adb', ['shell', 'settings', 'put', 'global', 'animator_duration_scale', '1.0']);
+    } else {
+      print("Creating screenshot using FLUTTER DRIVER");
+      final pixels = await driver.screenshot();
+      await file.writeAsBytes(pixels);
+    }
+    
     if (!silent) print('Screenshot $name created');
   } else {
     if (!silent) print('Warning: screenshot $name not created');
